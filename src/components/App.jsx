cb86614685b27ac34cf1f3b0de2100aa600659d6
EbrahimCoder <105370028+EbrahimCoder@users.noreply.github.com>
2022-05-18 06:54:09 +0530

    Added complex state management using objects.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,19 @@ function App() {
     email: ""
   });
 
+  function updateContact(event) {
+    const { name, value } = event.target;
+
+    /* Another version of setState that takes an argument prevState
+       which holds the previous state of the app */
+    setContact((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      };
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +27,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={updateContact}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={updateContact}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={updateContact}
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
